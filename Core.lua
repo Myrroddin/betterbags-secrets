3ae78a318005b1876425a90060ada157ea01d3ce
Myrroddin <psvander@gmail.com>
2026-06-16 23:21:21 -0600

    Bump ToC for patch 12.0.7

--- Core.lua
+++ Core.lua
@@ -6,44 +6,45 @@ local BetterBags = LibStub("AceAddon-3.0"):GetAddon("BetterBags")
 assert(BetterBags, "BetterBags_Secrets requires BetterBags")
 
 local categories = BetterBags:GetModule("Categories")
-
 local BL = BetterBags:GetModule("Localization")
+local context = BetterBags:GetModule("Context")
+local ctx = context:New("BetterBags_Secrets")
 
 local _, addon = ...
 
-local L = addon.L -- get the plugin's localizatons
+local L = addon.L -- get the plugin's localizations
 
 -- Secrets of Azeroth
 for itemID in pairs(addon.secretsOfAzeroth) do
-	categories:AddItemToCategory(itemID, BL:G(L["Secrets: Azeroth"]))
+	categories:AddItemToCategory(ctx, itemID, BL:G(L["Secrets: Azeroth"]))
 end
 
 -- Kosumoth the Hungering
 for itemID in pairs(addon.kosumoth) do
-	categories:AddItemToCategory(itemID, BL:G(L["Secrets: Kosumoth"]))
+	categories:AddItemToCategory(ctx, itemID, BL:G(L["Secrets: Kosumoth"]))
 end
 
 -- Uuna
 for itemID in pairs(addon.uuna) do
-	categories:AddItemToCategory(itemID, BL:G(L["Secrets: Uuna"]))
+	categories:AddItemToCategory(ctx, itemID, BL:G(L["Secrets: Uuna"]))
 end
 
 -- treasures
 for itemID in pairs(addon.treasures) do
-	categories:AddItemToCategory(itemID, BL:G(L["Secrets: Treasures"]))
+	categories:AddItemToCategory(ctx, itemID, BL:G(L["Secrets: Treasures"]))
 end
 
 -- Honeyback Hive & Honeyback Harvester
 for itemID in pairs(addon.honeyback) do
-	categories:AddItemToCategory(itemID, BL:G(L["Honeyback Hive"]))
+	categories:AddItemToCategory(ctx, itemID, BL:G(L["Honeyback Hive"]))
 end
 
 -- mounts
 for itemID in pairs(addon.mounts) do
-	categories:AddItemToCategory(itemID, BL:G(L["Secrets: Mounts"]))
+	categories:AddItemToCategory(ctx, itemID, BL:G(L["Secrets: Mounts"]))
 end
 
 -- battle pets
 for itemID in pairs(addon.battlePets) do
-	categories:AddItemToCategory(itemID, BL:G(L["Secrets: Battle Pets"]))
+	categories:AddItemToCategory(ctx, itemID, BL:G(L["Secrets: Battle Pets"]))
 end
